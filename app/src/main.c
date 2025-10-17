1981b8d8b987be521b79d40781bb9a37e77ee7ff
Faysal Hussain <faysalhu4@gmail.com>
2025-10-16 20:16:59 -0600

    4-LED Blink

--- app/src/main.c
+++ app/src/main.c
@@ -6,23 +6,42 @@
 #include <zephyr/drivers/gpio.h>
 
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
+#define LED3_NODE DT_ALIAS(led3)
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 
 int main(void) {
-    int ret;
+    int ret, ret2, ret3, ret4;
 
     if (!gpio_is_ready_dt(&led0)) {
         return -1;
     }
+    if (!gpio_is_ready_dt(&led1)) {
+        return -1; }
+    if (!gpio_is_ready_dt(&led2)) {
+        return -1; }
+    if (!gpio_is_ready_dt(&led3)) {
+        return -1;
+    }
 
+    ret4 = gpio_pin_configure_dt(&led3, GPIO_OUTPUT_ACTIVE);
+    ret3 = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+    ret2 = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
     ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) {
-        return ret;
+    if (ret < 0 || ret2 < 0 || ret3 < 0 || ret4 < 0) {
+        return -1;
     }
 
     while (1) {
         gpio_pin_toggle_dt(&led0);
+        gpio_pin_toggle_dt(&led1);
+        gpio_pin_toggle_dt(&led2);
+        gpio_pin_toggle_dt(&led3);
 
         k_msleep(1000);
     }
